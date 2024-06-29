bec645c46a5aabddd71a8b5f64435ca50705311e
Se-bastian <p.sebastian.23.11.99@gmail.com>
2024-06-29 16:40:36 -0500

    Funciones de encriptar incompleto

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,11 @@ function encriptar() {
 
     let texto_salida = document.getElementById('cuadro_salida');
 
-    texto_salida.innerHTML = palabraAUnicode(texto);
+    texto_salida.innerHTML = hola(texto);
+    //texto_salida.innerHTML = String.fromCharCode(texto.charCodeAt(0) + 3);
+    for (let palabra of  parrafoAUnicode(texto)) {
+        console.log(palabra);
+    }
 
     // Limpia el textarea de entrada.
     //document.getElementById("cuadro_entrada").value = '';
@@ -60,7 +64,7 @@ function cambiarInterfaz() {
  * y retornará una lista de números -los números son 
  * la representación unicode de cada letra.
  * @param {string} palabra 
- * @returns {listaNumero}
+ * @returns {Array}
  */
 function palabraAUnicode(palabra) {
     let listaNumero = []
@@ -76,5 +80,53 @@ function palabraAUnicode(palabra) {
     return listaNumero;
 }
 
+/**
+ * Esta función pide como parametro un string que
+ * se encargará, miestras la función "palabraAUnicode()"
+ * se encargará de separa en plabras y te devolverá
+ * un array[][] con las palabraas en unicode.
+ * @param {string} parrafo 
+ * @returns {Array}
+ */
+function parrafoAUnicode(parrafo) {
+    let tmpParrafo = parrafo.split(' ');
+    let listaUnicode = [];
 
-function 
+    for (let palabra of tmpParrafo) {
+        listaUnicode.push(palabraAUnicode(palabra));
+    }
+
+    return listaUnicode;
+}
+
+/**
+ * Usaremos el encriptador de El Cesar en el cual
+ * se basa en ponde la tercera letra superior de la
+ * letra actual.
+ * @param {string} palabra 
+ * @returns {string}
+ */
+function encriptacionDeCesar(palabra) {
+    let nuevaPalabra = '';
+
+    for (let letra of palabra) {
+        if ( letra !== ' ') {
+            let uniCode = letra.charCodeAt(0);
+            uniCode += 3;
+            nuevaPalabra.concat(String.fromCharCode(uniCode));
+        }else {
+            nuevaPalabra.concat(' ');
+        }
+    }
+
+    return nuevaPalabra;
+}
+
+function hola(palabra) {
+    let cadena = '';
+    for (let valor of palabra) {
+        cadena.concat(valor);
+    }
+
+    return cadena;
+}
