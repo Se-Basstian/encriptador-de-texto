276bf74f4569d7a44a0f226e9cbcc5cba39acaf6
Se-bastian <p.sebastian.23.11.99@gmail.com>
2024-06-30 15:47:31 -0500

    Boton copiar implementado

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 
-function encriptar() {
+function btn_encriptar() {
     let texto = document.getElementById("cuadro_entrada").value;
 
     cambiarInterfaz();
@@ -7,15 +7,31 @@ function encriptar() {
     let texto_salida = document.getElementById('cuadro_salida');
 
     texto_salida.innerHTML = encriptacionDeCesar(texto);
-    //texto_salida.innerHTML = String.fromCharCode(texto.charCodeAt(0) + 3);
-    for (let palabra of  parrafoAUnicode(texto)) {
-        console.log(palabra);
-    }
-
-    // Limpia el textarea de entrada.
-    //document.getElementById("cuadro_entrada").value = '';
+    
+    document.getElementById('cuadro_entrada').value = ''; // Linea encargada de limpiar el textarea de entrada de datos.
 }
 
+function btn_desEncriptacion() {
+    let texto = document.getElementById('cuadro_entrada').value;
+
+    cambiarInterfaz();
+
+    let texto_salida = document.getElementById('cuadro_salida');
+
+    texto_salida.innerHTML = desEncriptacion(texto);
+
+    document.getElementById('cuadro_entrada').value = ''; // Limpia la patalla de entrada.
+}
+
+function btn_copiar() {
+    let cuadro_salir = document.getElementById('cuadro_salida').value;
+
+    cambiarInterfaz2();
+
+    document.getElementById('cuadro_entrada').value = cuadro_salir;
+
+
+}
 
 /**
  * Oculta un elemento html que
@@ -26,13 +42,12 @@ function ocultarPorID(identificador) {
     document.getElementById(identificador).setAttribute('hidden', 'true');
 }
 
-
 /**
  * Muestra un elemento html que
  * se encontraba oculto.
  * @param {string} identificador 
  */
-function mostarPorID(identificador){
+function mostrarPorID(identificador){
     document.getElementById(identificador).removeAttribute('hidden')
 }
 
@@ -45,7 +60,6 @@ function limparValorElemeno(identificador) {
     document.getElementById(identificador).innerHTML = '';
 }
 
-
 /**
  * La función se encarga de ocultar y mostras
  * algunos elementos de la interfas gráfica.
@@ -54,8 +68,16 @@ function cambiarInterfaz() {
     ocultarPorID("muñeco");
     ocultarPorID("aviso_salida_titulo");
     ocultarPorID("aviso_salida_parrafo");
-    mostarPorID("btn_copiar");
-    mostarPorID("cuadro_salida");
+    mostrarPorID("btn_copiar");
+    mostrarPorID("cuadro_salida");
+}
+
+function cambiarInterfaz2() {
+    mostrarPorID("muñeco");
+    mostrarPorID("aviso_salida_titulo");
+    mostrarPorID("aviso_salida_parrafo");
+    ocultarPorID("btn_copiar");
+    ocultarPorID("cuadro_salida");
 }
 
 
@@ -74,10 +96,30 @@ function encriptacionDeCesar(palabra) {
             let uniCode = letra.charCodeAt(0);
             uniCode += 3;
             nuevaPalabra += String.fromCharCode(uniCode);
-        }else {
+        } else {
             nuevaPalabra += ' ';
         }
     }
 
+    return nuevaPalabra;
+}
+
+/**
+ * Desencriptador del método Cesar
+ * @param {string} palabra
+ * @returns {string}
+ */
+function desEncriptacion(palabra) {
+    let nuevaPalabra = ' ';
+
+    for (let letra of palabra) {
+        if (letra !== ' ') {
+            let uniCode = letra.charCodeAt(0);
+            uniCode -= 3;
+            nuevaPalabra += String.fromCharCode(uniCode);
+        } else {
+            nuevaPalabra += ' ';
+        }
+    }
     return nuevaPalabra;
 }
